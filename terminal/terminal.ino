0bd9f276e53ff5859ec25554c2fd7f3621c3f9dc
Jason Traud <jason.traud@outlook.com>
2023-09-09 15:05:53 -0400

    Update terminal.ino
    Prints firmware version string when the command "ver" is received. Otherwise print "NR" for Not Recognized.

--- terminal/terminal.ino
+++ terminal/terminal.ino
@@ -1,18 +1,33 @@
-unsigned char buffer[512];
-unsigned int bufferIndex = 0;
+unsigned char buffer[512];        // Buffer variables
+unsigned int bufferIndex = 0;     // 
 
-unsigned char delimiter = 0x0D;
-bool packetDetected = 0;
+unsigned char delimiter = 0x0D;   // Carriage return for our delimiter
+char* commandDelimiter = " ,=";   // Breaking up commands, accept a space, a comma, or an equals sign
+unsigned char whiteSpace = 0x20;  // White space
+bool packetDetected = 0;          // Flag for detecting a packet
+
+String firmwareVersion = "v1.0a"; // Hard coded firmware version
+
+// Our list of commands
+String command_firmwareVersion = "ver";
 
 void processPacket() {
-  unsigned int index = 0;
 
-  while(index < bufferIndex){
-    Serial.print((char)buffer[index]);
-    delay(10);
-    index++;
+  // Retrieve our command
+  String token;
+  token = strtok(buffer,commandDelimiter);
+
+  // Look for our stock commands
+  if (token.equalsIgnoreCase(command_firmwareVersion)) {
+    Serial.println(firmwareVersion);
   }
 
+  // Print "NR" for Not Recognized if the commands are not found
+  else {
+    Serial.println("NR");
+  }
+
+  // Reset our buffer
   bufferIndex = 0;
 }
 
@@ -23,8 +38,11 @@ void processIncomingSerial() {
     incomingByte = Serial.read();
     buffer[bufferIndex] = incomingByte;
 
-    if (incomingByte == delimiter)
+    if (incomingByte == delimiter) {
       packetDetected = 1;
+      buffer[bufferIndex] = 0x00;
+    }
+      
 
     bufferIndex ++;
   }
